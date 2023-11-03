6500109db44b301cd82730563ef459d0586b8cc0
lukky <lukkytung@gmail.com>
2023-11-03 21:11:28 +0800

    Checkin

--- flutter_mixed_native_code/android/app/src/main/kotlin/com/example/flutter_mixed_native_code/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_mixed_native_code
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- home_widget/android/app/src/main/kotlin/com/example/home_widget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.home_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
