0da8065ad84a509616a0f1a98a8a3a22d0070089
lukky <lukkytung@gmail.com>
2024-03-26 22:31:02 +0800

    Checkin

--- flutter_mvc/lib/main.dart
+++ flutter_mvc/lib/main.dart
@@ -12,12 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter MVC Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
-      home: const HomeView(title: 'Flutter Demo Home Page'),
+      home: const HomeView(title: 'Flutter MVC Home Page'),
     );
   }
 }
