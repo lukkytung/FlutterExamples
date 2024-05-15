3dad84809005de7b135d4b92a9a6a9c1748cb23a
Lukky <lukkytung@gmail.com>
2024-05-16 01:24:27 +0800

    Checkin

--- flutter_isolate/lib/provider/riverpod.dart
+++ flutter_isolate/lib/provider/riverpod.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_isolate/uitls/timer_notifier.dart';
+import 'package:flutter_isolate/provider/timer_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final countRiverpod = StateProvider<int>((ref) => 0);
