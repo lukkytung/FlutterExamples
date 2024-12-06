c0b3ec654db2ca1e91b163916114778ff285da45
Lukky <118007019+lukkytung@users.noreply.github.com>
2024-12-06 16:56:04 +0800

    Checkin

--- easy_localization_demo/lib/main.dart
+++ easy_localization_demo/lib/main.dart
@@ -67,6 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            
             Text('hello').tr(),
             const Text(
               'You have pushed the button this many times:',
